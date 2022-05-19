d6d6211af2760a3ebc71f912e452b8e6e5a8aecc
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-05-19 16:35:01 +0200

    Completato

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
+<!-- Descrizione:
+Creare una variabile con un paragrafo di testo a vostra scelta.
+Stampare a schermo il paragrafo e la sua lunghezza.
+Una parola da censurare viene passata dall'utente tramite parametro GET.
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare -->
+
+<?php
+
+$text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, eius.";
+//var_dump($text);
+//var_dump(strlen($text));
+
+
+//parola passata dall'utente tramite parametro GET.
+$wordSearch = $_GET['word'];
+//var_dump($wordSearch);
+$change_word = str_replace($_GET['word'], "***", $text);
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,17 +27,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Bad Words</title>
 </head> 
+    <style>
+    body {background-color: #DAF7A6;}
+    h5   {color: blue;}
+    </style>
 <body>
 
-<?php
 
-$text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, eius.";
-var_dump(strlen($text))
+<!-- Stampare a schermo il paragrafo e la sua lunghezza.-->
+<h4><?php echo $text; ?></h4>
+<h5>La lunghezza: <?php echo strlen($text); ?></h5>
 
-?>
-
-
-
+<h4><?php echo $change_word; ?></h4>
+<h5>La lunghezza: <?php echo strlen($change_word); ?></h5>
 
 
     
