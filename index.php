96943c8098451a557fcbcd23763349795652b712
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-05-19 15:39:58 +0200

    lunghezza del paragrafo

--- index.php
+++ index.php
@@ -5,8 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Bad Words</title>
-</head>
+</head> 
 <body>
+
+<?php
+
+$text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, eius.";
+var_dump(strlen($text))
+
+?>
+
 
 
 
